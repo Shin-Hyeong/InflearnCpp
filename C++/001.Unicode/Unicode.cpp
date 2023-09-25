bfefa6a4b2f7639f6326c760c37d3b502c3f81e3
ShinHyeong <struct4996@naver.com>
2023-09-25 16:40:44 +0900

    Unicode( UTF8 or UTF16le )

--- C++/001.Unicode/Unicode.cpp
+++ C++/001.Unicode/Unicode.cpp
@@ -4,16 +4,16 @@ using namespace std;
 int main()
 {
     char ch = 'a';
-    // wchar_t wch = L'안';
+    wchar_t wch = L'안';
     // 인코딩이 UFT-8 or UFT-16이여야함.
     // 문자앞에 L를 붙여줘야함
 
     cout << ch << endl;
     // cout는 char 전용
 
-    // wcout.imbue(locale("kor"));
+    wcout.imbue(locale("kor"));
     //  유니코드에서 한국어라는 인식시킴
-    // wcout << wch << endl;
+    wcout << wch << endl;
 
     return 0;
 }
